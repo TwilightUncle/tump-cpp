ff24400059366ef5acbb5e07c502a960d9634465
mizuno <mifukata-nozumi@outlook.jp>
2023-10-06 22:22:20 +0900

    式による表示に変更

--- test/containers_test.cpp
+++ test/containers_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <tump/metafunction/expression.hpp>
 #include <tump/containers.hpp>
 
 TEST(TumpContainersTest, FunctorTest)
@@ -6,54 +7,42 @@ TEST(TumpContainersTest, FunctorTest)
     // 第一則(それぞれ生成された関数実行結果の比較)
     using functor1 = tump::add_const;
     constexpr auto case1 = std::is_same_v<
-        tump::invoke_t<
-            tump::fmap_t<tump::type_identity, functor1>,
-            int
-        >,
-        tump::invoke_t<
-            std::type_identity_t<functor1>,
-            int
-        >
+        tump::exp<tump::fmap, tump::type_identity, functor1, tump::_doll, int>,
+        tump::exp<tump::type_identity, functor1, tump::_doll, int>
     >;
     ASSERT_TRUE(case1);
 
     // リストの第一則
     using functor2 = tump::list<int, short, long>;
     constexpr auto case2 = std::is_same_v<
-        tump::fmap_t<tump::type_identity, functor2>,
-        std::type_identity_t<functor2>
+        tump::exp<tump::fmap, tump::type_identity, functor2>,
+        tump::exp<tump::type_identity, functor2>
     >;
     ASSERT_TRUE(case2);
 
     // 関数の第二則
     using functor3 = tump::type_identity;
     constexpr auto case3 = std::is_same_v<
-        tump::invoke_t<
-            tump::fmap_t<tump::compose_t<
-                tump::add_const,
-                tump::make_unsigned
-            >, functor3>,
-            int
-        >,
-        tump::invoke_t<
-            tump::invoke_t<tump::compose_t<
-                tump::bind<tump::fmap, tump::add_const>,
-                tump::bind<tump::fmap, tump::make_unsigned>
-            >, functor3>,
-            int
-        >
+        tump::exp<tump::exp<
+            tump::fmap, tump::_doll,
+            tump::add_const, tump::_dot, tump::make_unsigned
+        >, functor3, tump::_doll, int>,
+        tump::exp<tump::exp<
+            tump::fmap, tump::add_const, tump::_dot,
+            tump::fmap, tump::make_unsigned
+        >, functor3, tump::_doll, int>
     >;
     ASSERT_TRUE(case3);
 
     // リストの第二則
     constexpr auto case4 = std::is_same_v<
-        tump::fmap_t<tump::compose_t<
-            tump::add_const,
-            tump::make_unsigned
+        tump::exp<tump::exp<
+            tump::fmap, tump::_doll,
+            tump::add_const, tump::_dot, tump::make_unsigned
         >, functor2>,
-        tump::invoke_t<tump::compose_t<
-            tump::bind<tump::fmap, tump::add_const>,
-            tump::bind<tump::fmap, tump::make_unsigned>
+        tump::exp<tump::exp<
+            tump::fmap, tump::add_const, tump::_dot,
+            tump::fmap, tump::make_unsigned
         >, functor2>
     >;
     ASSERT_TRUE(case4);
