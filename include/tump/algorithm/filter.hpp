bfc42d88e7734592bd51bb16f5efe178c12f06f0
mizuno <mifukata-nozumi@outlook.jp>
2023-10-09 02:30:34 +0900

    include漏れ修正

--- include/tump/algorithm/filter.hpp
+++ include/tump/algorithm/filter.hpp
@@ -1,6 +1,7 @@
 #ifndef TUMP_INCLUDE_GUARD_TUMP_ALGORITHM_FILTER_HPP
 #define TUMP_INCLUDE_GUARD_TUMP_ALGORITHM_FILTER_HPP
 
+#include TUMP_COMMON_INCLUDE(metafunction/invoke_list.hpp)
 #include TUMP_COMMON_INCLUDE(algorithm/push.hpp)
 
 namespace tump
