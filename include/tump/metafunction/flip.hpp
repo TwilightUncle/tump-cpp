59fb9cef8aa8da1cd3d0996e01c18057df9191a5
mizuno <mifukata-nozumi@outlook.jp>
2023-10-20 01:48:28 +0900

    制約修正

--- include/tump/metafunction/flip.hpp
+++ include/tump/metafunction/flip.hpp
@@ -10,7 +10,7 @@ namespace tump
         /**
          * 2引数を受け取るコールバック化したメタ関数に対し、引数の順序を入れ替えた上で実行  
         */
-        template <Invocable F, class L, class R>
+        template <InvocableArgN<2> F, class L, class R>
         using flip = invoke<F, R, L>;
     }
 
@@ -22,13 +22,13 @@ namespace tump
     /**
      * 2引数を受け取るコールバック化したメタ関数に対し、引数の順序を入れ替えた上で実行  
     */
-    template <Invocable F, class L, class R>
+    template <InvocableArgN<2> F, class L, class R>
     using flip_t = typename fn::flip<F, L, R>::type;
 
     /**
      * 2引数を受け取るコールバック化したメタ関数に対し、引数の順序を入れ替えた上で実行  
     */
-    template <Invocable F, class L, class R>
+    template <InvocableArgN<2> F, class L, class R>
     constexpr auto flip_v = fn::flip<F, L, R>::value;
 }
 
