b3bc97f0aa5e20f893cbcf758f0900a74be046d0
mizuno <mifukata-nozumi@outlook.jp>
2024-11-07 22:22:45 +0900

    fix: 足りていないinclude追加

--- include/tump.hpp
+++ include/tump.hpp
@@ -8,5 +8,6 @@
 #include TUMP_V_0_1_0_COMMON_INCLUDE(metafunction.hpp)
 #include TUMP_V_0_1_0_COMMON_INCLUDE(algorithm.hpp)
 #include TUMP_V_0_1_0_COMMON_INCLUDE(containers.hpp)
+#include TUMP_V_0_1_0_COMMON_INCLUDE(expression.hpp)
 
 #endif
